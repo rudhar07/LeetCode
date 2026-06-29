7272a252d048818f81b0f2a1460225a129e177e8
Rudhar Bajaj <rudharbajaj@gmail.com>
2026-06-29 05:48:05 +0530

    Time: 0 ms (100%), Space: 12.5 MB (1.96%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n<=2:
+            return n
+        
+        prev2 = 1
+        prev1 = 2
+
+        for i in range(3, n+1):
+            cur = prev2+prev1
+            prev2 = prev1
+            prev1 = cur
+        
+        return prev1
+
+        
+        
