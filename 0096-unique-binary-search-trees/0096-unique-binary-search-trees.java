06e8074d5d73ca106964ce1edc3f23a9999dc7b4
Rudhar Bajaj <rudharbajaj@gmail.com>
2026-06-29 05:52:18 +0530

    Time: 0 ms (100%), Space: 41.8 MB (83.14%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numTrees(int n) {
+        if(n<=2)return n;
+        int[] t = new int[n+1];
+        t[0] = t[1] = 1;
+        
+        
+        for(int i=2; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                t[i] += t[j-1] * t[i-j];
+            }
+        }
+        
+        return t[n];
+    }
+}
